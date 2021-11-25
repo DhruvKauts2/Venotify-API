287f64c7535a025484014b64eb0e079074267147
DhruvKauts2 <kautsdhruv15@gmail.com>
2021-11-25 17:16:19 +0300

    test

--- app.js
+++ app.js
@@ -1,120 +1,120 @@
 const express = require('express')  // Because we need express
 const app = express()   //Instantiate express and call the object "app"
-const mongoose = require('mongoose')    //Because we need mongoose for the database
-const bodyParser = require('body-parser');
-const SignUp = require('./models/signUp')
-const Login = require('./models/login')
-const Announcement = require('./models/announcement')
-app.use(bodyParser.json());
+// const mongoose = require('mongoose')    //Because we need mongoose for the database
+// const bodyParser = require('body-parser');
+// const SignUp = require('./models/signUp')
+// const Login = require('./models/login')
+// const Announcement = require('./models/announcement')
+// app.use(bodyParser.json());
 
 const port = process.env.port || 5000
-const DB = "mongodb+srv://Dhruv:Dhruv123@cluster0.bxgwe.mongodb.net/Venotify?retryWrites=true&w=majority"   //Database link + authentication
+// const DB = "mongodb+srv://Dhruv:Dhruv123@cluster0.bxgwe.mongodb.net/Venotify?retryWrites=true&w=majority"   //Database link + authentication
 
-mongoose.connect(DB, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => { //This function connects to the database
-    console.log('connection successful');
-  }).catch((err)=>console.log(err));
+// mongoose.connect(DB, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => { //This function connects to the database
+//     console.log('connection successful');
+//   }).catch((err)=>console.log(err));
 
 
 // ROUTE FOR SIGN UP PAGE
-app.post('/SignUp', async(req,res) => {
-  const signUpAs = req.body["SignUpAs"];
-  const name = req.body["Name"];
-  const sid = req.body["Sid"];
-  const phoneNumber = req.body["PhoneNumber"];
-  const email = req.body["Email"];
-  const branch = req.body["Branch"];
-  const password = req.body["Password"];
+// app.post('/SignUp', async(req,res) => {
+//   const signUpAs = req.body["SignUpAs"];
+//   const name = req.body["Name"];
+//   const sid = req.body["Sid"];
+//   const phoneNumber = req.body["PhoneNumber"];
+//   const email = req.body["Email"];
+//   const branch = req.body["Branch"];
+//   const password = req.body["Password"];
 
-  const records = await SignUp.findOne({Sid:sid});
-  console.log(records)
-  if(records == null || records.length == 0){
-    const newEntry = {SignUpAs:signUpAs, Name:name, Sid:sid, PhoneNumber:phoneNumber, Email:email, Branch:branch, Password:password};
-    const response = await SignUp.create(newEntry)
-    console.log(response)
-    res.json({
-      message: 'Created'
-    })
-  }
+//   const records = await SignUp.findOne({Sid:sid});
+//   console.log(records)
+//   if(records == null || records.length == 0){
+//     const newEntry = {SignUpAs:signUpAs, Name:name, Sid:sid, PhoneNumber:phoneNumber, Email:email, Branch:branch, Password:password};
+//     const response = await SignUp.create(newEntry)
+//     console.log(response)
+//     res.json({
+//       message: 'Created'
+//     })
+//   }
 
-  else{
-    res.status(403).json({
-      message: 'Already There'
-    })
-  }
-})
+//   else{
+//     res.status(403).json({
+//       message: 'Already There'
+//     })
+//   }
+// })
 
 
-//ROUTE FOR LOGIN PAGE
-app.post('/Login', async(req,res) => {
-  const name = req.body["Name"];
-  const password = req.body["Password"];
+// //ROUTE FOR LOGIN PAGE
+// app.post('/Login', async(req,res) => {
+//   const name = req.body["Name"];
+//   const password = req.body["Password"];
 
-  const records = await Login.findOne({Name : name})
-  console.log(records)
-  if(records == null || records.length == 0){
-    // const newEntry = {Username: userName, Password: password}
-    // const response = await login.create(newEntry)
-    // console.log(response)
-    res.json({
-      message: 'No ID exists. Please signup.'
-    })
-  }
+//   const records = await Login.findOne({Name : name})
+//   console.log(records)
+//   if(records == null || records.length == 0){
+//     // const newEntry = {Username: userName, Password: password}
+//     // const response = await login.create(newEntry)
+//     // console.log(response)
+//     res.json({
+//       message: 'No ID exists. Please signup.'
+//     })
+//   }
 
-  else{
-    res.status(200).json({
-      message: 'Logged in'
-    })
-  }
+//   else{
+//     res.status(200).json({
+//       message: 'Logged in'
+//     })
+//   }
  
-})
+// })
 
 
 
-//ROUTE FOR CREATE ANNOUNCEMENT PAGE
-app.post('/Announcement', async(req,res) => {
-  const organiser = req.body["Organiser"];
-  const time = req.body["Time"];
-  const date = req.body["Date_"];
-  const place = req.body["Place"];
-  const desciption = req.body["Description"];
+// //ROUTE FOR CREATE ANNOUNCEMENT PAGE
+// app.post('/Announcement', async(req,res) => {
+//   const organiser = req.body["Organiser"];
+//   const time = req.body["Time"];
+//   const date = req.body["Date_"];
+//   const place = req.body["Place"];
+//   const desciption = req.body["Description"];
 
-  const newEntry = {
-    Organiser : organiser,
-    Time : time,
-    Date_ : date,
-    Place : place,
-    Description : desciption
-  };
-  const response = await Announcement.create(newEntry)
-  console.log(response)
-  res.json({
-    message: 'Announcment Created'
-  })
-})
+//   const newEntry = {
+//     Organiser : organiser,
+//     Time : time,
+//     Date_ : date,
+//     Place : place,
+//     Description : desciption
+//   };
+//   const response = await Announcement.create(newEntry)
+//   console.log(response)
+//   res.json({
+//     message: 'Announcment Created'
+//   })
+// })
 
-//ROUTE FOR HOME PAGE
-app.get('/HomePage', async(req,res)=>{
-  const records = await Announcement.find()
-  console.log(records);
-  res.json(records)
-  //res.send('welcome')
-})
-
-
-//DATE FILTER 
-app.post('/HomePage/Date', async(req,res)=>{
-  const date = req.body["Date"]
-  const records = await Announcement.find({Date_ : date})
-  if(records == null || records.length == 0 ){
-    res.json({
-      message : "No Events Found"
-    })
-  }else {
-    res.json(records)
-  }
-})
+// //ROUTE FOR HOME PAGE
+// app.get('/HomePage', async(req,res)=>{
+//   const records = await Announcement.find()
+//   console.log(records);
+//   res.json(records)
+//   //res.send('welcome')
+// })
 
 
+// //DATE FILTER 
+// app.post('/HomePage/Date', async(req,res)=>{
+//   const date = req.body["Date"]
+//   const records = await Announcement.find({Date_ : date})
+//   if(records == null || records.length == 0 ){
+//     res.json({
+//       message : "No Events Found"
+//     })
+//   }else {
+//     res.json(records)
+//   }
+// })
+
+app.get('/Hi', (req,res)=>{res.send("Hi")})
 
 app.listen(port,() => { //This function activates the server
     console.log(`server is running on port ${port}`);
