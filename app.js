26d5496e3e93b79e3c0c040e79091653eabe615f
DhruvKauts2 <kautsdhruv15@gmail.com>
2021-11-25 16:41:28 +0300

    port change

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const Login = require('./models/login')
 const Announcement = require('./models/announcement')
 app.use(bodyParser.json());
 
-const port = process.env.PORT || 5000;
+const port = process.env.port;
 const DB = "mongodb+srv://Dhruv:Dhruv123@cluster0.bxgwe.mongodb.net/Venotify?retryWrites=true&w=majority"   //Database link + authentication
 
 mongoose.connect(DB, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => { //This function connects to the database
